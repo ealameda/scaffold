33db7fa33f89f5592b14ddf6ce8ef7b7bea48a8f
Eldon Alameda <eldon.alameda@lexisnexis.com>
2021-02-07 13:21:29 -0500

    Refactoring to remove global var

--- main.go
+++ main.go
@@ -15,54 +15,63 @@ type cfg struct {
 	Static   bool
 }
 
-var out io.Writer = os.Stdout
-
 func main() {
-	err := run()
+	config, err := setupScaffoldConfig(os.Stdout, os.Args[1:])
 	if err != nil {
-		for _, e := range err {
-			fmt.Fprintln(out, e.Error())
-		}
+		fmt.Println(err)
+		os.Exit(1)
 	}
+
+	exitIfAnyErrors(validateConfig(config))
+
+	generateScaffold(os.Stdout, config)
 }
 
-func run() []error {
+func validateConfig(config cfg) []error {
 	var allErrors []error
-
-	var scaffold = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
-	scaffold.SetOutput(out)
-
-	name := scaffold.String("n", "", "Project name")
-	location := scaffold.String("l", "", "Project location on disk")
-	url := scaffold.String("r", "", "Project remote repository url")
-	static := scaffold.Bool("s", false, "Project will have static assets or not")
-	err := scaffold.Parse(os.Args[1:])
-	if err != nil {
-		return nil
-	}
-
-	if *name == "" {
+	if config.Name == "" {
 		allErrors = append(allErrors, errors.New("Project name cannot be empty"))
 	}
 
-	if *location == "" {
+	if config.Location == "" {
 		allErrors = append(allErrors, errors.New("Project path cannot be empty"))
 	}
 
-	if *url == "" {
+	if config.Url == "" {
 		allErrors = append(allErrors, errors.New("Project repository URL cannot be empty"))
 	}
-	if len(allErrors) > 0 {
-		return allErrors
-	}
 
-	config := cfg{
-		Name:     *name,
-		Location: *location,
-		Url:      *url,
-		Static:   *static,
+	return allErrors
+}
+
+func setupScaffoldConfig(out io.Writer, args []string) (cfg, error) {
+	config := cfg{}
+	scaffold := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	scaffold.SetOutput(out)
+	scaffold.StringVar(&config.Name, "n", "", "Project name")
+	scaffold.StringVar(&config.Location, "l", "", "Project location on disk")
+	scaffold.StringVar(&config.Url, "r", "", "Project remote repository url")
+	scaffold.BoolVar(&config.Static, "s", false, "Project will have static assets or not")
+	err := scaffold.Parse(args)
+	if err != nil {
+		return config, err
 	}
+	if scaffold.NArg() != 0 {
+		return config, errors.New("No positional parameters expected")
+	}
+	return config, err
+
+}
+
+func generateScaffold(out io.Writer, config cfg) {
 	fmt.Fprintf(out, "Generating scaffold for project %s in %s\n", config.Name, config.Location)
-	return nil
+}
 
+func exitIfAnyErrors(errors []error) {
+	if len(errors) != 0 {
+		for _, e := range errors {
+			fmt.Println(e)
+		}
+		os.Exit(1)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,48 +1,90 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"github.com/stretchr/testify/assert"
-	"os"
 	"testing"
 )
 
-func TestScaffold(t *testing.T) {
-	out = bytes.NewBuffer(nil)
-
-	t.Run("should display an information message if all fields are provided", func(t *testing.T) {
-		os.Args = []string{"./scaffold", "-n", "Project1", "-l", "./project1", "-r", "github.com/username/project1"}
-		out = bytes.NewBuffer(nil)
-		main()
-		assert.Equal(t, "Generating scaffold for project Project1 in ./project1\n", out.(*bytes.Buffer).String())
+func Test_validateConfig(t *testing.T) {
+	t.Run("should return no errors with a valid config", func(t *testing.T) {
+		config := cfg{Name: "test", Url: "http://www.test.com", Location: "./project"}
+		errors := validateConfig(config)
+		assert.Len(t, errors, 0)
 	})
 
-	t.Run("should display errors if missing name", func(t *testing.T) {
-		os.Args = []string{"./scaffold", "-l", "./project1", "-r", "github.com/username/project1"}
-		out = bytes.NewBuffer(nil)
-		main()
-		assert.Equal(t, "Project name cannot be empty\n", out.(*bytes.Buffer).String())
+	t.Run("should return missing name if project name isn't set", func(t *testing.T) {
+		config := cfg{Url: "http://www.test.com", Location: "./project"}
+		errors := validateConfig(config)
+		assert.Len(t, errors, 1)
+		assert.Error(t, errors[0], "Project name cannot be empty")
 	})
 
-	t.Run("should display errors if missing location", func(t *testing.T) {
-		os.Args = []string{"./scaffold", "-n", "Project1", "-r", "github.com/username/project1"}
-		out = bytes.NewBuffer(nil)
-		main()
-		assert.Equal(t, "Project path cannot be empty\n", out.(*bytes.Buffer).String())
+	t.Run("should return missing path if project location isn't set", func(t *testing.T) {
+		config := cfg{Name: "test", Url: "http://www.test.com"}
+		errors := validateConfig(config)
+		assert.Len(t, errors, 1)
+		assert.Error(t, errors[0], "Project path cannot be empty")
 	})
 
-	t.Run("should display errors if missing repository URL", func(t *testing.T) {
-		os.Args = []string{"./scaffold", "-n", "Project1", "-l", "./project1"}
-		out = bytes.NewBuffer(nil)
-		main()
-		assert.Equal(t, "Project repository URL cannot be empty\n", out.(*bytes.Buffer).String())
+	t.Run("should return missing location if project url isn't set", func(t *testing.T) {
+		config := cfg{Name: "test", Location: "./project"}
+		errors := validateConfig(config)
+		assert.Len(t, errors, 1)
+		assert.Error(t, errors[0], "Project repository URL cannot be empty")
 	})
 
-	t.Run("should display multiple errors if both missing name and location", func(t *testing.T) {
-		os.Args = []string{"./scaffold", "-r", "github.com/username/project1"}
-		out = bytes.NewBuffer(nil)
-		main()
-		assert.Equal(t, "Project name cannot be empty\nProject path cannot be empty\n", out.(*bytes.Buffer).String())
+	t.Run("should return multiple errors with an empty config", func(t *testing.T) {
+		config := cfg{}
+		errors := validateConfig(config)
+		assert.Len(t, errors, 3)
+	})
+
+}
+
+func Test_generateScaffold(t *testing.T) {
+	t.Run("should generate a message that the scaffold was generated based on the config", func(t *testing.T) {
+		config := cfg{Name: "Project1", Url: "http://www.test.com", Location: "./project1"}
+		byteBuf := new(bytes.Buffer)
+		out := bufio.NewWriter(byteBuf)
+
+		generateScaffold(out, config)
+		out.Flush()
+		assert.Equal(t, "Generating scaffold for project Project1 in ./project1\n", byteBuf.String())
+	})
+}
+
+func Test_setupScaffoldConfig(t *testing.T) {
+	t.Run("Should populate a config based on the input", func(t *testing.T) {
+		args := []string{"-n", "project1", "-l", "/project1/", "-r", "github.com/ealameda/project1"}
+		out := new(bytes.Buffer)
+		//byteBuf.String()
+		config, error := setupScaffoldConfig(out, args)
+		assert.NoError(t, error)
+		assert.Equal(t, "project1", config.Name)
+		assert.Equal(t, "/project1/", config.Location)
+		assert.Equal(t, "github.com/ealameda/project1", config.Url)
+		assert.False(t, config.Static)
+	})
+
+	t.Run("should output help", func(t *testing.T) {
+		args := []string{"-h"}
+		out := new(bytes.Buffer)
+		config, err := setupScaffoldConfig(out, args)
+		assert.Error(t, err, "help requested")
+		assert.Empty(t, config)
+		assert.Contains(t, out.String(), "Usage of")
+
+	})
+
+	t.Run("should output errors", func(t *testing.T) {
+		args := []string{"foo bar"}
+		out := new(bytes.Buffer)
+		config, err := setupScaffoldConfig(out, args)
+		assert.Empty(t, config)
+		assert.Error(t, err, "no positional arguments provided")
+
 	})
 
 }
